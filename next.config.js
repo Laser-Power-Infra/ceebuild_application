2e7155a8e4e60bfa470defb972738c2b5049aa32
AI SERVER <asmita.mallick@laserpowrinfra.com>
2026-08-31 12:20:55 +0530

    added standalone in next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   // For Docker: set OUTPUT=standalone in Docker build env.
   // For local dev/prod: leave unset so next start works normally.
-  ...(process.env.NEXT_OUTPUT === 'standalone' ? { output: 'standalone' } : {}),
+  // ...(process.env.NEXT_OUTPUT === 'standalone' ? { output: 'standalone' } : {})
+  output:"standalone",
 };
 
 module.exports = nextConfig;
